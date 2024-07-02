e80df7b18353163a238467c330b29b150fbd34a9
andres <andresxv@gmail.com>
2024-07-02 11:43:41 -0300

    full front beams minimal implementation

--- static/script1.js
+++ static/script1.js
@@ -1,33 +1,47 @@
 
-async function doBeamsAuth() {
-    let beamsTokenProvider = new PusherPushNotifications.TokenProvider({
-        url: 'http://localhost:32000/tecniko-api/beams-auth'
-    });
+//import * as PusherPushNotifications from "@pusher/push-notifications-web";
 
+async function doBeamsAuth() 
+{
     let beamsClient = new PusherPushNotifications.Client({
         instanceId: '',
     });
 
+    let beamsTokenProvider = new PusherPushNotifications.TokenProvider({
+        url: 'http://localhost:32000/tecniko-api/beams-auth', // en la respuesta de este endpoint debe venir el token que se debe pasar en auth bearer
+    });
+ 
     try {
-        let startResult = await beamsClient.start();
-        console.log('Beams client started', startResult);
-        
-        let setUserResult = await beamsClient.setUserId('12', beamsTokenProvider);
+        await beamsClient.start();
+        let setUserResult = await beamsClient.setUserId('12', beamsTokenProvider); // An object that contains a method called fetchToken. This method must return a promise that resolves to a correctly signed Beams Token for the desired user ID (
         console.log('Beams client setUserId', setUserResult);
-        
     } catch (error) {
         console.error(error);
     }
 
-    /*beamsClient
-        .start() // This succesfully vapid key
-        .then((x)=>{
-            console.log('Beams client started', x)
-            beamsClient.setUserId('12', beamsTokenProvider)
-        })
-        .catch(console.error);*/
-    
-
 }
 
-doBeamsAuth();
+async function getBeamsUser() 
+{
+    let beamsClient = new PusherPushNotifications.Client({
+        instanceId: '',
+    });
+
+    // Get User ID
+    let userId = await beamsClient.getUserId();
+    console.log('Beams client userId', userId);
+    
+    // Get Device ID
+    let deviceId = await beamsClient.getDeviceId();
+    console.log('Beams client deviceId', deviceId);
+}
+
+async function stopBeamsAuth() 
+{
+    let beamsClient = new PusherPushNotifications.Client({
+        instanceId: '',
+    });
+
+    await beamsClient.stop();
+    console.log('Beams client stopped');
+}
